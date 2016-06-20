42133775c10a6011ba956ae61366a2c8cae1b6c4
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-06-19 22:11:27 -0500

    Control de flujo

---
+++ flujo/for.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+for x in xrange(1, 11):
+    for y in xrange(1, 11):
+        print '%d * %d = %d' % (x, y, x * y)
+
+for x in xrange(3):
+    print "Numero: " + str(x)
+    if x == 1:
+        break
+
+for x in xrange(3):
+    print x
+else:
+    print 'Final x = %d' % (x)
+
+string = "Hello World"
+
+for x in string:
+    print x
+
+collection = ['hey', 5, 'd']
+
+for x in collection:
+    print x
+
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for lista in list_of_lists:
+    for x in lista:
+        print x
